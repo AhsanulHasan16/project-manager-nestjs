0d75bd8d25766dba9c5c986430f8c9aeedab0afd
Ahsanul Hasan <82327664+AhsanulHasan16@users.noreply.github.com>
2025-02-26 20:00:52 +0600

    Implemented Swagger
    Added Swagger implementation for documentation.

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -3,27 +3,38 @@ import { TaskService } from './task.service';
 import { Task } from './task.entity';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CreateTaskDto } from './create-task.dto';
+import { ApiTags, ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('tasks')
+@ApiBearerAuth()
 @Controller('tasks')
 @UseGuards(JwtAuthGuard)
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
+  @ApiOperation({ summary: 'Get all tasks' })
+  @ApiResponse({ status: 200, description: 'Return all tasks.' })
   @Get()
   findAll(): Promise<Task[]> {
     return this.taskService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get a specific task by ID' })
+  @ApiResponse({ status: 200, description: 'Return a task.' })
   @Get(':id')
   findOne(@Param('id') id: number): Promise<Task> {
     return this.taskService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Create a new task' })
+  @ApiResponse({ status: 201, description: 'The task has been successfully created.' })
   @Post()
   create(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     return this.taskService.create(createTaskDto);
   }
 
+  @ApiOperation({ summary: 'Delete a task by ID' })
+  @ApiResponse({ status: 204, description: 'The task has been successfully deleted.' })
   @Delete(':id')
   remove(@Param('id') id: number): Promise<void> {
     return this.taskService.remove(id);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,21 +1,30 @@
 import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
+import { ApiTags, ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('users')
+@ApiBearerAuth()
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({ status: 200, description: 'Return all users.' })
   @Get()
   findAll(): Promise<User[]> {
     return this.userService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get a specific user by ID' })
+  @ApiResponse({ status: 200, description: 'Return a user.' })
   @Get(':id')
   findOne(@Param('id') id: number): Promise<User> {
     return this.userService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Delete a new user by ID' })
+  @ApiResponse({ status: 204, description: 'The user has been successfully deleted.' })
   @Delete(':id')
   remove(@Param('id') id: number): Promise<void> {
     return this.userService.remove(id);
